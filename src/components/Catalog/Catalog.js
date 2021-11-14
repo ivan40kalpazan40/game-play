bc1cfbfcfaf81d712bc1b64c8ea7ef529fe7f707
Ivan Skordev <maymunskoa@yahoo.com>
2021-11-14 02:07:15 +0200

    loading state in catalog

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -3,17 +3,23 @@ import CatalogItem from './CatalogItem';
 
 const Catalog = () => {
   const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
       .then((res) => res.json())
       .then((response) => {
         setGames(response);
+        setLoading(false);
       });
   }, []);
   return (
     <section id='catalog-page'>
       <h1>All Games</h1>
-      {games.length > 0 ? (
+      {loading ? (
+        <h3 className='no-articles'>Loading...</h3>
+      ) : games.length > 0 ? (
         games.map((item) => <CatalogItem item={item} key={item._id} />)
       ) : (
         <h3 className='no-articles'>No articles yet</h3>
